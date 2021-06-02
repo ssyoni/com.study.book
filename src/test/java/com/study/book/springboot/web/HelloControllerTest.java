7edd893714b62e5faf805278d54ef83fd7159c90
ssyoni <jsy950601@gmail.com>
2021-06-02 23:51:05 +0900

    Create HelloControllerTest.java

---
+++ src/test/java/com/study/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.study.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_return() throws Exception{
+        String hello ="hello";
+
+        mvc.perform(get("/hello")) // "hello" 주소로 HTTP GET 요청
+                .andExpect(status().isOk()) // HTTP header 의 Status를 검증
+                .andExpect(content().string(hello)); // Controller 에서 "hello"를 리턴 했는지
+    }
+}
