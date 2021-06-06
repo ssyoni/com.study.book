b7c4228bd7af0853805deb819043e25c25f32fde
ssyoni <jsy950601@gmail.com>
2021-06-06 22:05:52 +0900

    등록/수정/조회 API

---
+++ src/main/java/com/study/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package com.study.book.springboot.service.posts;
+
+import com.study.book.springboot.posts.Posts;
+import com.study.book.springboot.posts.PostsRepository;
+import com.study.book.springboot.web.dto.PostsResponseDto;
+import com.study.book.springboot.web.dto.PostsSaveRequestDto;
+import com.study.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
+    }
+}
